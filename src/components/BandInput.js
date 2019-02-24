3595b1afa052565be069b2ca0e3971db64d33193
magic8baller <lturis@gmail.com>
2019-02-23 23:55:01 -0500

    mostly makes sense..?

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,39 @@
-// Add BandInput component
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = e => {
+    this.setState({ name: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.addBand(this.state)
+    this.setState({ name: '' })
+  }
   render() {
-    return(
+    return (
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Band Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
-
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addBand: formData => dispatch({ type: 'ADD_BAND', payload: formData })
+//   }
+// }
 export default BandInput
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
-
+import BandInput from '../components/BandInput'
+import { connect } from 'react-redux'
+import Bands from '../components/Bands'
 class BandsContainer extends Component {
   render() {
-    return(
+    const { addBand, bands } = this.props
+    return (
       <div>
-        BandsContainer
+        <BandInput addBand={addBand} />
+        <hr />
+        <h2>bands:</h2>
+        <Bands bands={bands} />
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => ({ bands: state.bands })
+const mapDispatchToProps = dispatch => ({
+  addBand: band =>
+    dispatch({
+      type: 'ADD_BAND',
+      band
+    })
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BandsContainer)
